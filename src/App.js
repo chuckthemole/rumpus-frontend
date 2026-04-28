6e8837f6c84da46cbd963900a5ac05ca49ac9983
Chuck Thomas <chuckthemole@gmail.com>
2026-04-28 12:26:49 -0400

    Add EventLoggerProvider

--- src/App.js
+++ src/App.js
@@ -10,7 +10,8 @@ import {
     ColorSettingsProvider,
     RumpusModalProvider,
     LOGGER,
-    LocalPersistence
+    LocalPersistence,
+    EventLoggerProvider
 } from '@rumpushub/common-react';
 
 export default function App() {
@@ -57,21 +58,27 @@ export default function App() {
                             }
                         }}
                     >
+
                         <RumpusModalProvider appElement={appElement}>
+
                             <AuthRoot className="app-inner">
-                                <Header
-                                    header_path={"/view/header"}
-                                />
+                                <EventLoggerProvider>
+                                    <Header
+                                        header_path={"/view/header"}
+                                    />
 
-                                <main className="app-content columns is-centered">
-                                    <div className="column"></div>
-                                    <LayoutContent />
-                                    <div className="column"></div>
-                                </main>
+                                    <main className="app-content columns is-centered">
+                                        <div className="column"></div>
+                                        <LayoutContent />
+                                        <div className="column"></div>
+                                    </main>
 
-                                <Footer footer_path={"/view/footer"} />
+                                    <Footer footer_path={"/view/footer"} />
+                                </EventLoggerProvider>
                             </AuthRoot>
+
                         </RumpusModalProvider>
+
                     </FontSettingsProvider>
                 </ColorSettingsProvider>
             </div>
